f9f452270ad0d2ad1d18774d212b63ba8ea80c67
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-03-06 14:44:12 +0100

    $*

--- src/app/models/video.class.ts
+++ src/app/models/video.class.ts
@@ -1,7 +1,7 @@
 export interface VideoQuality {
     quality: string;
     video_file: string;
-  }
+}
 
 
 export class Video {
@@ -16,34 +16,37 @@ export class Video {
     thumbnail: string;
     view_count: number;
     qualities?: VideoQuality[];
+    film_rating: number;
 
-    constructor(obj?:any){
-        this.id = obj ? obj.id: null;
-        this.created_at = obj ? obj.created_at: null;
-        this.created_from = obj ? obj.created_from: null;
-        this.title = obj ? obj.title: '';
-        this.description = obj ? obj.description: '';
-        this.category = obj ? obj.category: '';
-        this.video_file = obj ? obj.video_file: '';
-        this.likes = obj ? obj.likes: [];
-        this.thumbnail = obj ? obj.thumbnail: '';
-        this.view_count = obj ? obj.view_count: null;
+    constructor(obj?: any) {
+        this.id = obj ? obj.id : null;
+        this.created_at = obj ? obj.created_at : null;
+        this.created_from = obj ? obj.created_from : null;
+        this.title = obj ? obj.title : '';
+        this.description = obj ? obj.description : '';
+        this.category = obj ? obj.category : '';
+        this.video_file = obj ? obj.video_file : '';
+        this.likes = obj ? obj.likes : [];
+        this.thumbnail = obj ? obj.thumbnail : '';
+        this.view_count = obj ? obj.view_count : null;
         this.qualities = obj ? obj.qualities : [];
+        this.film_rating = obj ? obj.film_rating : null;
     }
 
-    public toJSON(){
+    public toJSON() {
         return {
             id: this.id,
             created_at: this.created_at,
-           created_from: this.created_from,
-           title: this.title,
-           description: this.description,
-           category: this.category,
-           video_file: this.video_file,
-           likes: this.likes,
-           thumbnail: this.thumbnail,
-           view_count: this.view_count,
-           qualities: this.qualities
+            created_from: this.created_from,
+            title: this.title,
+            description: this.description,
+            category: this.category,
+            video_file: this.video_file,
+            likes: this.likes,
+            thumbnail: this.thumbnail,
+            view_count: this.view_count,
+            qualities: this.qualities,
+            film_rating: this.film_rating,
         }
     }
 }
